a6cf5863a0464143ae6e45839402feef6bf6e48b
lilei3 <lilei3@le.com>
2017-07-03 14:48:07 +0800

    021_fin_3

--- 021.c
+++ 021.c
@@ -1,28 +1,56 @@
+/* my own 1, past in first round */
 struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-    struct ListNode *p = l1;
-    struct ListNode *q = l2;
     struct ListNode *new = (struct ListNode *)malloc(sizeof(struct ListNode));
     struct ListNode *start = new;
     new->next = NULL;
-    while(p && q) {
-        if (q->val <= p->val) {
-            new->next = q;
-            q = q->next;
+    while(l1 && l2) {
+        if (l1->val <= l2->val) {
+            new->next = l1;
+            l1 = l1->next;
         } else {
-            new->next = p;
-            p = p->next;
+            new->next = l2;
+            l2 = l2->next;
         }
         new = new->next;
     }
 
-    if (p) {
-        new->next = p;
+    if (l1) {
+        new->next = l1;
     }
 
-    if (q) {
-        new->next = q;
+    if (l2) {
+        new->next = l2;
     }
 
     return start->next;
 }
 
+/* second version */
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
+    if (!l1)   return l2;
+    if (!l2)   return l1;
+    if (l1->val <= l2->val) {
+        l1->next = mergeTwoLists(l1->next, l2);
+        return l1;
+    } else {
+        l2->next = mergeTwoLists(l1, l2->next);
+        return l2;
+    }
+}
+
+
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+
+        if(l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+    }
+};
