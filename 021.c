b17b44772404f298f467f95cae1a1cd07ea9de3b
lilei3 <lilei3@le.com>
2017-07-03 14:15:02 +0800

    021_init_accept_2

--- 021.c
+++ 021.c
@@ -8,11 +8,9 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
         if (q->val <= p->val) {
             new->next = q;
             q = q->next;
-            new->next->next = NULL;
         } else {
             new->next = p;
             p = p->next;
-            new->next->next = NULL;
         }
         new = new->next;
     }
