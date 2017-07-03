806b41db3f8255f7fc7de0b0206821c721421b53
lilei3 <lilei3@le.com>
2017-07-03 11:38:38 +0800

    019_better_fin

--- 019.c
+++ 019.c
@@ -33,3 +33,48 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
 
     return head;
 }
+
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+    int i = 0;
+    int dpoint = 0;
+    struct ListNode *start = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode *fast = start;
+    struct ListNode *slow = start;
+    start->next = head;
+
+    while (i <= n) {
+        fast = fast -> next;;
+        i++;
+    }
+
+    while (fast) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    slow->next = slow->next->next;
+
+    return start->next;
+}
+
+
+
+public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode start = new ListNode(0);
+    ListNode slow = start, fast = start;
+    slow.next = head;
+
+    //Move fast in front so that the gap between slow and fast becomes n
+    for(int i=1; i<=n+1; i++)   {
+        fast = fast.next;
+    }
+    //Move fast to the end, maintaining the gap
+    while(fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    //Skip the desired node
+    slow.next = slow.next.next;
+    return start.next;
+}
